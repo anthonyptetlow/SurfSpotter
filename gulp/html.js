6a0a0527e133d10e1fa5c0297abb92ef8bc00491
anthonyptetlow <anthonyptetlow@gmail.com>
2015-07-24 14:00:31 +0100

    Added minification to html

--- gulp/html.js
+++ gulp/html.js
@@ -1,6 +1,9 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+	minifyHTML = require('gulp-minify-html'),
+    util = require('gulp-util');
 
 gulp.task('html', function() {
 	return gulp.src(__dirname + '/../src/**/*.html')
+        .pipe(!!util.env.production ? minifyHTML() : util.noop())
 		.pipe(gulp.dest(__dirname + '/../public/'));
 });
