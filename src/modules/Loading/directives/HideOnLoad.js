c6aff0cf0a95fe3dbde384af8ef1166602742801
anthonyptetlow <anthonyptetlow@gmail.com>
2015-07-25 00:28:14 +0100

    Fixing loader bug'

--- src/modules/Loading/directives/HideOnLoad.js
+++ src/modules/Loading/directives/HideOnLoad.js
@@ -2,7 +2,7 @@ angular.module('surfspotter').directive('hideOnLoad', [
 	function(){
 		return {
 			priority: -1000,
-			controller: function($scope, $element) {
+			link: function($scope, $element) {
 				$scope.$on('$viewContentLoaded', function () {
 					$element.addClass('hide');
 				});
