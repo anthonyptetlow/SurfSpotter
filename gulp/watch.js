0e3793aa5b3640abc41652d306fa8cfe6cf9bcbb
anthonyptetlow <anthonyptetlow@gmail.com>
2015-08-21 13:15:14 +0100

    Fixing issue woht the watch task

--- gulp/watch.js
+++ gulp/watch.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 
 gulp.task('watch', function () {
 	gulp.watch(__dirname + '/../src/**/*.js', ['script']);
-	gulp.watch(__dirname + '/../src/**/*.html', ['html', 'critical']);
+	gulp.watch(__dirname + '/../src/**/*.html', ['html', 'script', 'critical']);
 	gulp.watch(__dirname + '/../src/less/**/*.less', ['styles', 'critical']);
 	gulp.watch(__dirname + '/../src/assets/*', ['assets']);
 });
