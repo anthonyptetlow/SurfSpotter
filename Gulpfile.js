91cf5082d086c4b78813821ccf546111a6f05d55
anthonyptetlow <anthonyptetlow@gmail.com>
2015-08-26 13:03:42 +0100

    Removed livereload

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,12 +1,3 @@
 var gulp = require('gulp'),
-	server = require('gulp-server-livereload'),
 	require('./gulp/build'),
 	require('./gulp/deploy');
-
-gulp.task('server', function() {
-  gulp.src('public')
-    .pipe(server({
-      // livereload: true,
-      port: 8000
-    }));
-});
