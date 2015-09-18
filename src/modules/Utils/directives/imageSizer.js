256555cbeff1015712da160bb2171ffe320ab9d0
anthonyptetlow <anthonyptetlow@gmail.com>
2015-09-18 18:54:59 +0100

    Added Presizing of images

---
+++ src/modules/Utils/directives/imageSizer.js
@@ -0,0 +1,28 @@
+angular.module('surfspotter').directive('image', [
+	function() {
+		// Runs during compile
+		return {
+			scope: {
+				url: '@',
+				imageWidth: '@',
+				imageHeight: '@'
+			},
+			// name: '',
+			// priority: 1,
+			// terminal: true,
+			// scope: {}, // {} = isolate, true = child, false/undefined = no change
+			// controller: function($scope, $element, $attrs, $transclude) {},
+			// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+			// restrict: 'A', // E = Element, A = Attribute, C = Class, M = Comment
+			// template: '',
+			// templateUrl: '',
+			// replace: true,
+			// transclude: true,
+			template: '<img class="img-responsive" data-ng-src="{{url}}" onerror="this.style.display=\'none\';">',
+			replace: true,
+			link: function($scope, element, attrs, controller) {
+				attrs.$set('height', element[0].offsetWidth * ($scope.imageHeight / $scope.imageWidth));
+			}
+		};
+	}
+]);
