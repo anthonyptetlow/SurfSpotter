2e1ea860230bfc373611034ba844e237dd85554e
anthonyptetlow <anthonyptetlow@gmail.com>
2015-09-27 19:23:36 +0100

    Added Social icons and added updated branding colors

--- gulp/build.js
+++ gulp/build.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+	util = require('gulp-util');
 require('./clean');
 require('./script');
 require('./html');
@@ -8,6 +9,18 @@ require('./lib');
 require('./watch');
 require('./critical');
 
-gulp.task('buildOnce', ['clean', 'lib', 'script', 'html', 'styles', 'assets', 'critical']);
-gulp.task('build', ['buildOnce', 'watch']);
+var buildTasks = [];
 
+buildTasks.push('clean');
+buildTasks.push('lib');
+buildTasks.push('script');
+buildTasks.push('html');
+buildTasks.push('styles');
+buildTasks.push('assets');
+
+if(!!util.env.production) buildTasks.push('critical');
+
+
+gulp.task('buildOnce', buildTasks);
+
+gulp.task('build', ['buildOnce', 'watch']);
