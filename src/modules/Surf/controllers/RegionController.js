66a0918ef9db0b7524f8512c0a6463702c3b08aa
anthonyptetlow <anthonyptetlow@gmail.com>
2015-09-30 13:31:34 +0100

    Tweaked styles and added metadat for region pags

--- src/modules/Surf/controllers/RegionController.js
+++ src/modules/Surf/controllers/RegionController.js
@@ -1,8 +1,16 @@
 angular.module('surfspotter').controller('RegionController', [
     'region',
     'locations',
-    function (region, locations) {
+    'MetadataService',
+    function (region, locations, MetadataService) {
         angular.extend(this, region);
         this.locations = locations;
+
+		MetadataService.setTitle(region.name + '\'s surf spots');
+		MetadataService.setDescription('A list of all the surf spots in ' + region.name + ', from here you can select a region and ');
+		MetadataService.addKeyword(region.name);
+		MetadataService.addKeyword('spots');
+		MetadataService.addKeyword('beach');
+		MetadataService.addKeyword('locations');
     }
 ]);
