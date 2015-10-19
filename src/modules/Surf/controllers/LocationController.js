c3ad8b3e4ac0aec1293684eefaa05b6f760b6bd8
anthonyptetlow <anthonyptetlow@gmail.com>
2015-10-19 13:29:09 +0100

    Various refinements of text and pages

--- src/modules/Surf/controllers/LocationController.js
+++ src/modules/Surf/controllers/LocationController.js
@@ -1,6 +1,13 @@
 angular.module('surfspotter').controller('LocationController', [
+    'MetadataService',
     'location',
-    function (location) {
+    function (MetadataService, location) {
         angular.extend(this, location);
+
+        MetadataService.setTitle('All surf spots');
+		MetadataService.setDescription('A list of all the surf spots on surf spotter. From here you can select a location and view it\'s surfing forecast');
+		MetadataService.addKeyword('spots');
+		MetadataService.addKeyword('beach');
+		MetadataService.addKeyword('locations');
     }
 ]);
--- src/modules/Surf/controllers/RegionController.js
+++ src/modules/Surf/controllers/RegionController.js
@@ -4,10 +4,10 @@ angular.module('surfspotter').controller('RegionController', [
     function (region, MetadataService) {
         angular.extend(this, region.region);
         this.locations = region.locations;
-
-		MetadataService.setTitle(region.name + '\'s surf spots');
-		MetadataService.setDescription('A list of all the surf spots in ' + region.name + ', from here you can select a region and ');
-		MetadataService.addKeyword(region.name);
+        console.log(region);
+		MetadataService.setTitle(this.name + '\'s surf spots');
+		MetadataService.setDescription('A list of all the surf spots in ' + this.name + ', from here you can select a location and view it\'s surfing forecast');
+		MetadataService.addKeyword(this.name);
 		MetadataService.addKeyword('spots');
 		MetadataService.addKeyword('beach');
 		MetadataService.addKeyword('locations');
